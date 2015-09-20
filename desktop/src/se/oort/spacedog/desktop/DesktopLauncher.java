90a0b1bd22f621e16e7d3ff1b964d0ef74abf01c
Martin Bruse <zondolfin@gmail.com>
2015-09-20 21:34:32 +0200

    Initial commit of empty libgdx project.

---
+++ desktop/src/se/oort/spacedog/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.oort.spacedog.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.oort.spacedog.SpaceDog;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceDog(), config);
+	}
+}

a3fb787e841ea1749eda59d01d4ed14094ca5f28
Martin Bruse <zondolfin@gmail.com>
2015-09-21 18:16:58 +0200

    some dynamics

--- core/src/se/oort/spacedog/SpaceDog.java
+++ core/src/se/oort/spacedog/SpaceDog.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.Bullet;
 import com.badlogic.gdx.physics.bullet.collision.CollisionObjectWrapper;
@@ -41,6 +42,12 @@ import com.badlogic.gdx.physics.bullet.collision.btManifoldPoint;
 import com.badlogic.gdx.physics.bullet.collision.btManifoldResult;
 import com.badlogic.gdx.physics.bullet.collision.btSphereBoxCollisionAlgorithm;
 import com.badlogic.gdx.physics.bullet.collision.btSphereShape;
+import com.badlogic.gdx.physics.bullet.dynamics.btConstraintSolver;
+import com.badlogic.gdx.physics.bullet.dynamics.btDiscreteDynamicsWorld;
+import com.badlogic.gdx.physics.bullet.dynamics.btDynamicsWorld;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
+import com.badlogic.gdx.physics.bullet.dynamics.btSequentialImpulseConstraintSolver;
+import com.badlogic.gdx.physics.bullet.linearmath.btMotionState;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.badlogic.gdx.utils.Disposable;
@@ -51,46 +58,74 @@ public class SpaceDog extends ApplicationAdapter {
     final static short OBJECT_FLAG = 1<<9;
     final static short ALL_FLAG = -1;
 
+    static class MyMotionState extends btMotionState {
+        Matrix4 transform;
+        @Override
+        public void getWorldTransform (Matrix4 worldTrans) {
+            worldTrans.set(transform);
+        }
+        @Override
+        public void setWorldTransform (Matrix4 worldTrans) {
+            transform.set(worldTrans);
+        }
+    }
+
     class MyContactListener extends ContactListener {
         @Override
         public boolean onContactAdded (int userValue0, int partId0, int index0, int userValue1, int partId1, int index1) {
-            instances.get(userValue0).moving = false;
-            instances.get(userValue1).moving = false;
+            if (userValue0 != 0)
+                ((ColorAttribute)instances.get(userValue0).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+            if (userValue1 != 0)
+                ((ColorAttribute)instances.get(userValue1).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
             return true;
         }
     }
 
     static class GameObject extends ModelInstance implements Disposable {
-        static class Constructor implements Disposable {
-            public final Model model;
-            public final String node;
-            public final btCollisionShape shape;
-            public Constructor(Model model, String node, btCollisionShape shape) {
-                this.model = model;
-                this.node = node;
-                this.shape = shape;
-            }
+        public final btRigidBody body;
+        public final MyMotionState motionState;
 
-            public GameObject construct() {
-                return new GameObject(model, node, shape);
-            }
-
-            @Override
-            public void dispose () {
-                shape.dispose();
-            }
-        }
-        public final btCollisionObject body;
-        public boolean moving;
-        public GameObject(Model model, String node, btCollisionShape shape) {
+        public GameObject (Model model, String node, btRigidBody.btRigidBodyConstructionInfo constructionInfo) {
             super(model, node);
-            body = new btCollisionObject();
-            body.setCollisionShape(shape);
+            motionState = new MyMotionState();
+            motionState.transform = transform;
+            body = new btRigidBody(constructionInfo);
+            body.setMotionState(motionState);
         }
 
         @Override
         public void dispose () {
             body.dispose();
+            motionState.dispose();
+        }
+
+        static class Constructor implements Disposable {
+            public final Model model;
+            public final String node;
+            public final btCollisionShape shape;
+            public final btRigidBody.btRigidBodyConstructionInfo constructionInfo;
+            private static Vector3 localInertia = new Vector3();
+
+            public Constructor (Model model, String node, btCollisionShape shape, float mass) {
+                this.model = model;
+                this.node = node;
+                this.shape = shape;
+                if (mass > 0f)
+                    shape.calculateLocalInertia(mass, localInertia);
+                else
+                    localInertia.set(0, 0, 0);
+                this.constructionInfo = new btRigidBody.btRigidBodyConstructionInfo(mass, null, shape, localInertia);
+            }
+
+            public GameObject construct () {
+                return new GameObject(model, node, constructionInfo);
+            }
+
+            @Override
+            public void dispose () {
+                shape.dispose();
+                constructionInfo.dispose();
+            }
         }
     }
 
@@ -124,7 +159,8 @@ public class SpaceDog extends ApplicationAdapter {
     MyContactListener contactListener;
 
     btBroadphaseInterface broadphase;
-    btCollisionWorld collisionWorld;
+    btDynamicsWorld dynamicsWorld;
+    btConstraintSolver constraintSolver;
 
     private void createEnvironment() {
         Bullet.init();
@@ -146,7 +182,9 @@ public class SpaceDog extends ApplicationAdapter {
         collisionConfig = new btDefaultCollisionConfiguration();
         dispatcher = new btCollisionDispatcher(collisionConfig);
         broadphase = new btDbvtBroadphase();
-        collisionWorld = new btCollisionWorld(dispatcher, broadphase, collisionConfig);
+        constraintSolver = new btSequentialImpulseConstraintSolver();
+        dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constraintSolver, collisionConfig);
+        dynamicsWorld.setGravity(new Vector3(0, -10f, 0));
         contactListener = new MyContactListener();
 
     }
@@ -176,45 +214,36 @@ public class SpaceDog extends ApplicationAdapter {
         model = mb.end();
 
         constructors = new ArrayMap<String, GameObject.Constructor>(String.class, GameObject.Constructor.class);
-        constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f))));
-        constructors.put("sphere", new GameObject.Constructor(model, "sphere", new btSphereShape(0.5f)));
-        constructors.put("box", new GameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f))));
-        constructors.put("cone", new GameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f)));
-        constructors.put("capsule", new GameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f)));
-        constructors.put("cylinder", new GameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f))));
+        constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f)), 0f));
+        constructors.put("sphere", new GameObject.Constructor(model, "sphere", new btSphereShape(0.5f), 1f));
+        constructors.put("box", new GameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)), 1f));
+        constructors.put("cone", new GameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f), 1f));
+        constructors.put("capsule", new GameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f), 1f));
+        constructors.put("cylinder", new GameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f)), 1f));
 
         instances = new Array<GameObject>();
         GameObject object = constructors.get("ground").construct();
         instances.add(object);
-        collisionWorld.addCollisionObject(object.body, GROUND_FLAG, ALL_FLAG);
+        dynamicsWorld.addRigidBody(object.body, GROUND_FLAG, ALL_FLAG);
 
     }
 
 	@Override
-	public void create () {
+	public void create() {
         createEnvironment();
         createModels();
     }
 
-	@Override
-	public void render () {
-        final float delta = Math.min(1f / 30f, Gdx.graphics.getDeltaTime() * 5);
+    @Override
+    public void render () {
+        final float delta = Math.min(1f / 30f, Gdx.graphics.getDeltaTime());
 
-        for (GameObject obj : instances) {
-            if (obj.moving) {
-                obj.transform.trn(0f, -delta, 0f);
-                obj.body.setWorldTransform(obj.transform);
-            }
-        }
-
-        collisionWorld.performDiscreteCollisionDetection();
-
+        dynamicsWorld.stepSimulation(delta, 5, 1f/60f);
 
         if ((spawnTimer -= delta) < 0) {
             spawn();
             spawnTimer = 1.5f;
         }
-
 
         camController.update();
 
@@ -226,17 +255,15 @@ public class SpaceDog extends ApplicationAdapter {
         modelBatch.end();
 	}
 
-    public void spawn() {
-        GameObject obj = constructors.values[1+MathUtils.random(constructors.size-2)].construct();
-        obj.moving = true;
+    public void spawn () {
+        GameObject obj = constructors.values[1 + MathUtils.random(constructors.size - 2)].construct();
         obj.transform.setFromEulerAngles(MathUtils.random(360f), MathUtils.random(360f), MathUtils.random(360f));
         obj.transform.trn(MathUtils.random(-2.5f, 2.5f), 9f, MathUtils.random(-2.5f, 2.5f));
-        obj.body.setWorldTransform(obj.transform);
+        obj.body.proceedToTransform(obj.transform);
         obj.body.setUserValue(instances.size);
         obj.body.setCollisionFlags(obj.body.getCollisionFlags() | btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
         instances.add(obj);
-        collisionWorld.addCollisionObject(obj.body, OBJECT_FLAG, GROUND_FLAG);
-
+        dynamicsWorld.addRigidBody(obj.body, OBJECT_FLAG, GROUND_FLAG);
     }
 
 	@Override
@@ -255,7 +282,8 @@ public class SpaceDog extends ApplicationAdapter {
         modelBatch.dispose();
         model.dispose();
         contactListener.dispose();
-        collisionWorld.dispose();
+        dynamicsWorld.dispose();
+        constraintSolver.dispose();
         broadphase.dispose();
     }
 
